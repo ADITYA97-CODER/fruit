41445b55d5559430c9d4cea8cc3337dbbe3cf009
oaditya97@gmail.com <oaditya97@gmail.com>
2022-11-05 10:45:48 -0700

    bug_fix

--- fruit/settings.py
+++ fruit/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w6co+go$2*$24_-x%l8@k*g^chqy^%4a82p@low&s106s#&u90'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
